ec5c7fcd7059f2ea3ba0ffdbb718e531330848b7
zhangc <zhangc@e8cdf396-7dcb-434b-ae6f-fcf53f1d1014>
2017-06-09 06:52:54 +0000

    城市联盟计划方案设置

---
+++ manage/language/zh_cn/cityunion.php
@@ -0,0 +1,24 @@
+<?php
+defined('InIMall') or exit('Access Invalid!');
+/**
+ * 推广体系 语言包
+ */
+$lang['cityunion_config']      = '城市联盟配置';
+$lang['cityunion_partner']      = '合伙人补贴';
+$lang['cityunion_seller']      = '商家补贴';
+$lang['cityunion_commislevel'] = '联盟佣金分配';
+$lang['cityunion_commisclass'] = '联盟佣金模板';
+
+$lang['partner_list']   = '合伙人管理';
+$lang['apply_list']       = '联盟申请';
+$lang['promotion_detail'] = '业绩明细';
+
+$lang['commis_detail']    = '抽佣明细';
+
+$lang['commisputout_list']    = '待结算佣金';
+$lang['commisputout_balance'] = '已结算佣金';
+$lang['commisputout_apply']   = '提佣申请';
+$lang['commisputout_detail']  = '佣金明细';
+
+$lang['commis_statistics'] = '抽佣统计';
+$lang['commis_paihang']    = '抽佣排行';
--- manage/modules/extension/include/menu.php
+++ manage/modules/extension/include/menu.php
@@ -1,31 +1,16 @@
 <?php
-/**
- * 推广系统菜单
- *
+/**
+ * 推广系统菜单
+ *
  */
-defined('InIMall') or exit('Access Invalid!');
-$_menu['extension'] = array (
-        'name' => '推广系统',
-        'child' => array(
-                array(
-                        'name' => '配置',
-                        'child' => array(
-                                'extension_config' => '推广配置'
-                        )
-                ),
-                array(
-                        'name' => '推广员',
-                        'child' => array(
-                                'extension_promotion' => '推广员管理'
-								
-                        )
-                ),
-                array(
-                        'name' => '结算',
-                        'child' => array(                                
-								'extension_commisdetail' => '抽佣明细',
-								'extension_commisputout' => '佣金结算',
-								'extension_statistics' => '抽佣统计'
-                        )
-                )
-));
+defined('InIMall') or exit('Access Invalid!');
+$_menu['extension'] = array (
+        'name' => '推广系统',
+        'child' => array(
+			array(
+            	'name' => '配置',
+            	'child' => array(            			'extension_config' => '推广配置',        				'cityunion_config' => '城市联盟设置',            	)
+			),
+            array(            	'name' => '推广员',            	'child' => array(            		'extension_promotion' => '推广员管理'            	)
+            ),        	array(        		'name' => '结算',        		'child' => array(        				'extension_commisdetail' => '抽佣明细',        				'extension_commisputout' => '佣金结算',        				'extension_statistics' => '抽佣统计'        			)        	)
+	));
