b6cbc0c8e2ad5e453eb640e909acdafed4734e07
huyh <huyh@e8cdf396-7dcb-434b-ae6f-fcf53f1d1014>
2017-06-10 03:35:42 +0000

    新加订货订单相关文件 data/model/ordering.model.php mobile/control/store_ordering.php

---
+++ data/model/ordering.model.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * 订单管理
+ * @copyright  Copyright (c) 2007-2016 zlin-e Inc. (http://www.zlin-e.com)
+ * @license    http://www.zlin-e.com
+ * @link       http://www.zlin-e.com
+ * @since      File available since Release v1.1
+ */
+defined('InIMall') or exit('Access Invalid!');
+class orderingModel extends Model {
+
+    public $count;//记录总条数
+    public $totalPage;//记录总页数
+
+	/**
+     * 取订单信息
+     *
+     * @param unknown_type $condition
+     * @param array $extend 追加返回那些表的信息,如array('order_common','order_goods','store')
+     * @return unknown
+     */
+    public function getOrderingInfo($condition = array(),$fields = '*', $order = '') {
+        $order_info = $this->table('ordering')->field($fields)->where($condition)->order($order)->select();
+        if (empty($order_info)) {
+            return array();
+        }
+        return $order_info;
+    }
+
+
+    /**
+     * 取得订货订单商品列表(按照商品公共id进行分组)
+     * @param unknown $condition
+     * @param string $pagesize
+     * @param string $field
+     * @param string $order
+     * @param string $limit
+     * @param unknown $extend 追加返回那些表的信息,如array('order_common','order_goods','store')
+     * @return Ambigous <multitype:boolean Ambigous <string, mixed> , unknown>
+     */
+    public function getOrderingList($condition, $pagesize = '', $field = '*',$group = '', $order = 'order_id desc', $limit = '', $extend = array(), $master = false){
+        //获取订货订单商品信息
+        $list = $this->table('ordering_goods')->field($field)->where($condition)->page($pagesize)->group($group)->order($order)->limit($limit)->master($master)->select();
+
+        //获取总条数
+        $this->count = count($this->table('ordering_goods')->field($field)->where($condition)->group($group)->order($order)->limit($limit)->master($master)->select());
+
+        //获取总页数
+        if(!$pagesize){//pagesize不存在时表示获取所有数据
+            $this->totalPage = 1;//默认页码为1
+        }else{
+           $this->totalPage = ceil($this->count/ $pagesize); 
+        }
+        
+
+        if (empty($list)) return array();
+        $order_list = array();
+        if (empty($order_list)) $order_list = $list;
+
+        
+        //追加返回商品公众表信息
+        if (in_array('goods_common',$extend)) {
+            $goods_commonid_array = array();
+            foreach ($order_list as $value) {
+                if (!in_array($value['goods_commonid'],$goods_commonid_array)) $goods_commonid_array[] = $value['goods_commonid'];
+            }
+            $goods_common_list = Model('goods_common')->getGoodsCommonList(array('goods_commonid'=>array('in',$goods_commonid_array)));
+            $goods_common_new_list = array();
+            foreach ($goods_common_list as $goods_common) {               
+                $goods_common_new_list[$goods_common['goods_commonid']] = $goods_common['goods_name'];
+            }
+            foreach ($order_list as $order_id => $order) {
+                $order_list[$order_id]['goods_common_name'] = $goods_common_new_list[$order['goods_commonid']];
+            }
+        }
+
+        return $order_list;
+    }
+
+
+
+}
---
+++ mobile/control/store_ordering.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * 商家订货
+ *
+ * @copyright  Copyright (c) 2007-2016 zlin-e Inc. (http://www.zlin-e.com)
+ * @license    http://www.zlin-e.com
+ * @link       http://www.zlin-e.com
+ * @since      File available since Release v1.1
+ */
+
+defined('InIMall') or exit('Access Invalid!');
+
+
+
+class store_orderingControl extends mobileHomeControl{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /**
+     * 获取订单列表
+     * 
+     * @return [type] [description]
+     */
+    /* 
+//我的订单 
+    请求参数：
+        size       传空值 
+        state_type 订单状态 表示未提交:state_new
+        order      排序字段 零售额:total_price 货号:goods_commonid 数量：num
+        sc         排序方式   正序：asc 倒序 ：desc
+        page       每页显示条数  默认10
+        curpage    当前页码  分页用
+    请求示例：
+        http://zlin.test.com/mobile/index.php?act=store_ordering&op=ordering_list&state_type=state_new&size=&order=total_price&sc=asc&page=3&curpage=1
+//总排行榜 
+    请求参数：
+        state_type 订单状态     不传默认所有订单
+        order      排序字段:    固定 num   表示按数量排序
+        sc         排序方式:    固定 desc  倒序
+        size       类型    ：   固定 total 表示总排行榜数据
+        page       每页显示条数 固定传0 表示获取所有数据 前台进行分页处理
+    请求示例：
+        http://zlin.test.com/mobile/index.php?act=store_ordering&op=ordering_list&state_type=&size=total&order=num&sc=desc&page=0&curpage=1
+//订单管理
+    已完成订单：
+http://zlin.test.com/mobile/index.php?act=store_ordering&op=ordering_list&state_type=state_commit&size=order&order=num&sc=desc&page=0&curpage=1
+
+总排行榜和我的订单点进去时 
+传值：
+订单状态(同总排行榜和我的订单的订单状态 前台传值)和商品公共id
+用户id  去获取商品id和数量
+     */
+    public function ordering_listOp(){
+        $model_order = Model('ordering');
+        $condition = array();
+        //订单状态        
+        $condition = $this->ordering_type_no($_GET["state_type"]);
+        //排序
+        $order = $_GET["order"];
+        $sc    = $_GET["sc"];
+        //类型 我的订单size 不传 总排行榜 size：total 订单管理 size:order 
+        $size  = $_GET["size"];
+        $page  = $_GET["page"];
+        if($page === '0'){//page传0时 表示获取所有数据
+            $page  = 0;
+        }else{
+            $page  = $_GET["page"]?$_GET["page"]:10;
+        }
+        
+        
+        $orderby = $order.' '.$sc;
+        $condition['buyer_id'] = 1;//$this->seller_info['seller_id'];//商家(登录者)id
+        if($size == 'total'){//请求总排行榜数据时 获取所有的状态的订单
+           unset($condition['ordering_state']); 
+        }
+        //获取订单信息
+        $ordering_info = $model_order->getOrderingInfo($condition,'*', 'ordering_id desc');
+
+       
+        if($ordering_info){
+            unset($condition);
+            //总排行榜时获取数据
+            if($size == 'total' && count($ordering_info) > 1){
+                foreach ($ordering_info as $key => $value) {
+                    $ordering_id[] = $value['ordering_id'];
+                }
+                $ordering_id = implode(',',$ordering_id);
+                $condition['ordering_id'] = array('in',$ordering_id);
+                $ordering_list = $model_order->getOrderingList($condition, $page, 'goods_commonid,goods_image,sum(goods_num) num,goods_price,goods_price*sum(goods_num) total_price,store_goods_state','goods_commonid', $orderby,'', array('goods_common'));
+            }else{
+            //其他请求时 获取数据(多获取了订单id ordering_id字段)
+                foreach ($ordering_info as $key => $value) {
+                    $condition['ordering_id'] = $value['ordering_id'];
+                    $ordering_list[] = $model_order->getOrderingList($condition, $page, 'ordering_id,goods_commonid,goods_image,sum(goods_num) num,goods_price,goods_price*sum(goods_num) total_price,store_goods_state','goods_commonid', $orderby,'', array('goods_common'));
+                }
+            }
+            
+          
+        }
+        $ordernum = count($ordering_list);
+        if($ordernum == 1 && $size!='order'){
+            $ordering_list = $ordering_list[0];
+        }elseif ($size == 'total') {
+            //总排行榜 添加排名
+            foreach ($ordering_list as $key => $value) {
+                $ordering_list[$key]['sort'] = $key+1;             
+            }
+            
+        }elseif ($size == 'order') {
+            //请求数据为订单管理时 数据处理
+            foreach ($ordering_list as $key => $value) {
+                $new_ordering_list[$key]['style_num'] = count($value);
+                foreach ($value as $k => $v) {
+                    $new_ordering_list[$key]['ordering_id'] = $v['ordering_id'];
+                    $new_ordering_list[$key]['piece_num'] += $v['num'];
+                    $new_ordering_list[$key]['total_price'] += $v['total_price'];
+                }
+            }
+            foreach ($new_ordering_list as $key => $value) {
+                $model = Model();
+                $ordeiing_info = $model->table('ordering')->where(array('ordering_id'=>$value['ordering_id']))->field("add_time")->find();
+                $new_ordering_list[$key]['addtime'] = date("Y-m-d H:i:s",$ordeiing_info['add_time']);
+                //订单管理 底部栏显示信息 合计  总金额
+                $bottom = array();
+                $bottom['total_style_num'] += $value['style_num'];
+                $bottom['total_piece_num'] += $value['piece_num'];
+                $bottom['total_price']     += $value['total_price'];
+                
+            }
+            $ordering_list = $new_ordering_list;
+
+            
+        }
+
+        $totalPage = $model_order->totalPage;//获取总页数
+
+        if($_GET["state_type"] == "state_commit"){
+            //当订单管理 是已完成的订单添加底部合计信息
+            output_data(array('ordering_list' => $ordering_list,'bottom'=>$bottom), mobile_page($totalPage));
+            exit;
+        }
+        output_data(array('ordering_list' => $ordering_list), mobile_page($totalPage));
+    }
+
+/**
+ * 订单状态
+ * @param  [type] $stage [订单状态]
+ * @return [type]        [description]
+ */
+    private function ordering_type_no($stage) { 
+        switch ($stage){
+            //订单状态：0(已取消)10(默认):未提交;20:未付款(已提交);21:已付款;22:已付定金;30:已发货;40:已收货
+            case 'state_new':
+                $condition['ordering_state'] = '10';
+                break;
+            case 'state_commit':
+                $condition['ordering_state'] = '20';
+                break;
+            case 'state_send':
+                $condition['ordering_state'] = '30';
+                break;
+            case 'state_noeval':
+                $condition['ordering_state'] = '40';
+                break;
+        }
+        return $condition;
+    }
+    
+}
