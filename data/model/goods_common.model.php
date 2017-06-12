0da8de82c87efe43207d98e0619241878fdf67a2
huyh <huyh@e8cdf396-7dcb-434b-ae6f-fcf53f1d1014>
2017-06-12 02:59:22 +0000

    data/model/goods_common.model.php 新增model

---
+++ data/model/goods_common.model.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 店铺模型管理
+ *
+ *
+ *
+ *
+ * @copyright  Copyright (c) 2007-2016 zlin-e Inc. (http://www.zlin-e.com)
+ * @license    http://www.zlin-e.com
+ * @link       http://www.zlin-e.com
+ * @since      File available since Release v1.1
+ */
+defined('InIMall') or exit('Access Invalid!');
+
+class goods_commonModel extends Model {
+
+    public function __construct() {
+        parent::__construct('goods_common');
+    }
+
+	/**
+	 * 查询查询商品公众表列表
+     *
+	 * @param array $condition 查询条件
+	 * @param int $page 分页数
+	 * @param string $order 排序
+	 * @param string $field 字段
+	 * @param string $limit 取多少条
+     * @return array
+	 */
+    public function getGoodsCommonList($condition, $page = null, $order = '', $field = '*', $limit = '') {
+        $result = $this->field($field)->where($condition)->order($order)->limit($limit)->page($page)->select();
+        return $result;
+    }
+
+}
