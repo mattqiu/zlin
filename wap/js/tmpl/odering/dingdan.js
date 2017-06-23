97ef9d0fd9b77334e313222ead6f492614785381
wangxiaofei45 <450731224@qq.com>
2017-06-23 16:55:03 +0800

    0.1

--- wap/js/tmpl/odering/dingdan.js
+++ wap/js/tmpl/odering/dingdan.js
@@ -69,38 +69,89 @@ $(function() {
 	})
 	//模拟数据的双向绑定
 	//第一步点击button的时侯找到相应的input输入框
-	$('.color_center button').click(function() {
-		console.log($(this).index())
+	//单选
 
-	})
 
-	$('.sizes_center button').click(function() {
-		console.log($(this).index())
-		bind_fortwo();
-	})
-
-	function bind_fortwo() {
-		for(var i = 0; i < $('.sizes_center button').length; i++) {
-			console.log($('.sizes_center').find('.button_active')[i])
-		}
-	};
-	bind_fortwo();
-	//第二部将数据绑定到相应的输入框里面
-
-	//第三步 获取相应的数据 并提交
-	//全选
+		var row = null;
+		var col = null;
+		$('.color_center button').click(function() {
+			col = $(this).index()
+			console.log(col);
+		})
+		$('.sizes_center button').click(function() {
+			var col = 2;
+			row = $(this).index();
+			console.log(row);
+			console.log(typeof(row))
+			//多选操作
+			$('.size_center .center_list').eq(row+1).find('input').addClass('active').parent().parent().siblings().find('input').removeClass('active');
+		})
+	//全选操作
 	var num = null;
 	$('.right .all').click(function() {
-		console.log("全选的操作")
 		if($('.color .right .all').hasClass('all_active') && $('.sizes .right .all').hasClass('all_active')) {
 			$('.size_center input').addClass('input_active')
-			var num = $('.num .num_center .right input').val();
-			console.log(num);
-			$('.size_center input').val(num);
+			num = $('.num .num_center .right input').val();
+			$('.size_center input').val(num)
+
 		} else {
 			$('.size_center input').removeClass('input_active')
 		}
 
 	})
+	//数量增加减少操作
+	$('.num .num_center .right .down').click(function() {
+		//判断是否选择的长度是否为0；如果是0;弹出提示框
+		if($('.sizes_center button.button_active').length == 0 || $('.color_center button.button_active').length == 0) {
+			$('.warning1').show();
+			$('.warning1').animate({
+				opacity: '1'
+			});
+			setTimeout(function() {
+				$('.warning1').animate({
+					opacity: '0'
+				});
+			}, 3000);
+		} else {
+			//判断如果长度不是0的操作;
+			if($('.num .num_center .right input').val() > 0) {
+				num--;
+				$('.num .num_center .right input').val(num);
+				if($('.color .right .all').hasClass('all_active') && $('.sizes .right .all').hasClass('all_active')) {
+					$('.size_center input').val(num)
+				}
+			} else {
+				$('.warning').show();
+				$('.warning').animate({
+					opacity: '1'
+				});
+				setTimeout(function() {
+					$('.warning').animate({
+						opacity: '0'
+					});
+				}, 3000);
+			}
+		}
 
+	})
+	$('.num .num_center .right .up').click(function() {
+		if($('.sizes_center button.button_active').length == 0 || $('.color_center button.button_active').length == 0) {
+			$('.warning1').show();
+			$('.warning1').animate({
+				opacity: '1'
+			});
+			setTimeout(function() {
+				$('.warning1').animate({
+					opacity: '0'
+				});
+			}, 3000);
+		} else {
+			num++;
+			$('.num .num_center .right input').val(num);
+			if($('.color .right .all').hasClass('all_active') && $('.sizes .right .all').hasClass('all_active')) {
+				$('.size_center input').val(num)
+			}
+		}
+
+	})
 })
